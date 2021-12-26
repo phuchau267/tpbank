1678c582f6892e2a02f2b28d8c5daf56b9b9093e
phuchau <tranphuchau9a9@gmail.com>
2021-12-26 14:34:18 +0700

    final

--- src/config/db/index.js
+++ src/config/db/index.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose');
-
+const {MONGO_HOST,MONGO_PORT} = require('../dbs-config');
  async function connect() {
      try {
-        await mongoose.connect('mongodb://mongodb:27017/tpbankfico', {
+        await mongoose.connect(`mongodb://${MONGO_HOST}:${MONGO_PORT}/tpbankfico`, {
          
         });
           console.log('connect success');
---
+++ src/config/dbs-config.js
@@ -0,0 +1,4 @@
+module.exports = {
+    MONGO_HOST: process.env.MONGO_HOST,
+    MONGO_PORT: process.env.MONGO_PORT
+}
