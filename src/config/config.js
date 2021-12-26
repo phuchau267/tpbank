3eedaa9281db52c54e69b0229f7b144c1a85d7c2
phuchau <tranphuchau9a9@gmail.com>
2021-12-26 22:24:10 +0700

    s

--- src/config/config.js
+++ src/config/config.js
@@ -2,11 +2,11 @@ module.exports = {
     HOME_TITLE: "Ngân Hàng Tiên Phong - Vay Tín Chấp Uy Tín Hàng Đầu - TPbank Fico",
     HOME_DESCRIPTION: "Hỗ trợ vay tín chấp tiêu dùng lãi suất thấp, các gói vay đa dạng như: vay tiền nhanh, vay trả góp, vay theo lương, vay theo bảo hiểm, vay theo hóa đơn tiện ích, vay sinh viên, vay theo hợp đồng vay cũ",
     HOME_KEYWORDS: "vay tín chấp tiêu dùng, vay lãi suất thấp, vay tiền nhanh, vay trả góp, vay theo lương, vay theo bảo hiểm, vay theo hóa đơn tiện ích, vay sinh viên, vay theo hợp đồng vay cũ",
-    HOME_URL: "https://tpbfico.vn",
+    HOME_URL: "http://tpbfico.vn",
     HOME_SITENAME: "tpbfico.vn",
   
     RECRUITMENT_TITLE: "Ngân Hàng Tiên Phong - Tuyển Dụng - TPbank Fico",
     RECRUITMENT_DESCRIPTION: "Ngần Hàng Tiên Phong TPBank tuyển dụng việc làm với nhiều ưu đãi hấp dẫn",
     RECRUITMENT_KEYWORDS: "việc làm, việc làm ngân hàng, tuyển dụng, tuyển dụng ngân hàng, xin việc làm, xin việc, việc làm tại ngân hàng, việc làm tpbank, xin việc tpbank, việc làm ngân hàng tiên phong, xin việc sale, việc làm sale, việc làm sale tại ngân hàng, việc làm sale tpbank",
-    RECRUITMENT_URL: "https://tpbfico.vn/recruitment",
+    RECRUITMENT_URL: "http://tpbfico.vn/recruitment",
 }
